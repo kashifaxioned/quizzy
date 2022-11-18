ada5319704b01d2c2635486f4b9a9d486522a934
kashifaxioned <kashifa@axioned>
2022-11-18 14:29:46 +0530

    home page layout added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        grey: "#282A3A",
+        gold: "#C69749 ",
+      },
+      fontFamily: {
+        rubik: ["Rubik Marker Hatch", "cursive"],
+      },
+      keyframes: {
+        'slideIn': {
+          '0%': { 'border': 'none' },
+          '100%': { 'border-bottom': '1px solid #C69749' }
+        }
+      }
+    },
+  },
+  plugins: [],
+};
