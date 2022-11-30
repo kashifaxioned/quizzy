25dfaca1b50b9bc20537a615c96b96a36cf2aa5c
kashifaxioned <kashifa@axioned>
2022-11-30 14:30:34 +0530

    404 error handling page added

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useGetQuestionsQuery } from "../features/api/apiSlice";
 import Home from "./Home/Home";
 import Questions from "./Questions/Questions";
+import PageNotFound from './404'
 
 export default function App() {
   const { data, isSuccess } = useGetQuestionsQuery();
   return (
-    <div>
+    <div className="font-rubik bg-grey text text-gold border-box">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -15,6 +16,7 @@ export default function App() {
             data.results.map((x, i) => {
               return <Route path={`question-${i+1}`} key={i} element={<Questions key={i} data={x} num={i+1}/>} />;
             })}
+          <Route path="*" element={<PageNotFound/>}/>
         </Routes>
       </Router>
     </div>
